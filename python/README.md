de3321de1ba38b56ef61330fdf5e3362008740f3
Keto Zhang <keto.zhang@gmail.com>
2020-10-04 00:28:08 +0000

    Add python

---
+++ python/README.md
@@ -0,0 +1,5 @@
+```sh
+bash install-pyenv.sh
+pyenv install -v x.y.z
+pyenv global x.y.z
+```
---
+++ python/install-pyenv.sh
@@ -0,0 +1,11 @@
+set -e
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+	libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+	xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+curl https://pyenv.run | bash
+echo "export PATH=\"$HOME/.pyenv/bin:$PATH\"" >> ~/.bashrc
+echo "eval \"$(pyenv init -)\"" >> ~/.bashrc
+echo eval "\"$(pyenv virtualenv-init -)\"" >> ~/.bashrc
+source ~/.bashrc
+
+pyenv update
